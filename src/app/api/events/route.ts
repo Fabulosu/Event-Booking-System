a402d9e3e894e2c334104b7e62236098248f0dc0
Edward <batalanedward@gmail.com>
2024-09-21 13:23:09 +0300

    added searchParams to the events api get route

--- src/app/api/events/route.ts
+++ src/app/api/events/route.ts
@@ -1,11 +1,29 @@
 import { dbConnect } from "@/utils/database";
 import { EventModel } from "@/utils/models";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(req: NextRequest) {
+
+    const url = new URL(req.url);
+    const category = url.searchParams.get("category");
+    const location = url.searchParams.get("location");
+    const name = url.searchParams.get("name");
+
     await dbConnect();
+    const query: any = {};
+
+    if (category) {
+        query.category = { $regex: category, $options: "i" };
+    }
+    if (location) {
+        query.location = { $regex: location, $options: "i" };
+    }
+    if (name) {
+        query.title = { $regex: name, $options: "i" };
+    }
+
     try {
-        const events = await EventModel.find({});
+        const events = await EventModel.find(query);
         return NextResponse.json(events, { status: 200 });
     } catch (error) {
         return NextResponse.json({ error: 'Failed to retrieve events' }, { status: 500 });
