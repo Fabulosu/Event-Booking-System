f80bf8cdf25f995903b3324671e987186ffed0a3
Edward <batalanedward@gmail.com>
2024-09-23 19:21:36 +0300

    replaced div with link, removed click handler, new location format

--- src/components/ui/event.tsx
+++ src/components/ui/event.tsx
@@ -2,14 +2,16 @@ import Image from 'next/image';
 import React from 'react';
 import { FaRegClock } from 'react-icons/fa';
 import { IoLocationOutline } from 'react-icons/io5';
+import Link from 'next/link';
 
 interface Props {
     data: any
 }
 
 const Event: React.FC<Props> = ({ data }) => {
+
     return (
-        <div key={data._id} className="h-[400px] flex flex-col justify-between hover:cursor-pointer transition-transform transform hover:scale-105 hover:z-10">
+        <Link key={data._id} href={`/event/${data._id}`} className="h-[400px] w-[400px] flex flex-col justify-between hover:cursor-pointer transition-transform transform hover:scale-105 hover:z-10">
             <div className="relative">
                 <Image
                     src="/images/mockhead.png"
@@ -22,12 +24,12 @@ const Event: React.FC<Props> = ({ data }) => {
                     <p className="text-xl font-bold">{new Date(data.date).getDate()}</p>
                     <p className="text-sm -mt-1 font-bold">{new Date(data.date).toLocaleString("en-US", { month: "short" })}</p>
                 </div>
-                <h3 className="text-3xl font-semibold mt-1">{data.title}</h3>
+                <h3 className="text-3xl font-semibold mt-1 text-wrap">{data.title}</h3>
             </div>
             <div className="flex flex-col">
                 <p className="font-bold text-lg">{data.price === 0 ? "FREE" : `$${data.price}`}</p>
                 <p className="text-gray-600 flex flex-row gap-1 items-center">
-                    <IoLocationOutline /> {data.location}
+                    <IoLocationOutline /> {data.location.split(',')[0]}
                 </p>
                 <p className="text-gray-600 flex flex-row gap-1 items-center">
                     <FaRegClock />
@@ -42,7 +44,7 @@ const Event: React.FC<Props> = ({ data }) => {
                     }).format(new Date(data.date))}
                 </p>
             </div>
-        </div>
+        </Link>
     )
 }
 
