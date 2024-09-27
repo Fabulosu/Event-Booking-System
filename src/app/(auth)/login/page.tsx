b5585a2200fcfc1b52a0315fe0f4548d30fa44dc
Edward <batalanedward@gmail.com>
2024-09-27 19:04:43 +0300

    responsive login & register pages

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -36,18 +36,20 @@ export default function LoginPage(props: Props) {
     }
 
     return (
-        <div className="h-full w-full flex justify-between bg-[#131619]">
-            <form onSubmit={onSubmit} className="flex flex-col justify-center items-center text-white w-full">
-                <div className="-mt-60 mb-60 w-[500px] flex flex-row items-center gap-5">
+        <div className="h-full w-full flex flex-col md:flex-row justify-between bg-[#131619]">
+            <form onSubmit={onSubmit} className="flex flex-col justify-center items-center text-white w-full h-full px-4 md:px-0">
+                <div className="mt-8 md:-mt-60 mb-12 md:mb-60 w-full md:w-[500px] flex flex-row items-center gap-3 justify-center md:justify-start">
                     <Image src="/images/logo.webp" alt="Logo" width={1024} height={1024} className="w-[40px] h-[40px] rounded-2xl" />
                     <p className="font-semibold text-2xl">SwiftSeats</p>
                 </div>
-                <div className="flex flex-col w-[500px] gap-20">
-                    <div className="flex flex-col gap-2">
-                        <h1 className="text-4xl font-semibold">Log into your account!</h1>
+
+                <div className="flex flex-col w-full md:w-[500px] gap-8 md:gap-20">
+                    <div className="flex flex-col gap-2 text-center md:text-left">
+                        <h1 className="text-3xl md:text-4xl font-semibold">Log into your account!</h1>
                         <p className="text-[#9B9C9E]">Let&apos;s remember your bookings!</p>
                     </div>
-                    <div className="flex flex-col w-[500px] gap-5">
+
+                    <div className="flex flex-col gap-5">
                         <div className="flex flex-col gap-1">
                             <p className="text-[#9B9C9E]">Email or username</p>
                             <Input type="text" name="username" placeholder="admin@fabulosu.xyz" className="text-black" ref={usernameRef} required={true} />
@@ -61,7 +63,11 @@ export default function LoginPage(props: Props) {
                     </div>
                 </div>
             </form>
-            <Image src="/images/illustration.webp" alt="Illustration" width={970} height={970} className="h-full w-full rounded-l-3xl" />
+
+            <div className="hidden lg:block w-auto lg:w-full">
+                <Image src="/images/illustration.webp" alt="Illustration" width={970} height={970} className="h-full w-full md:w-auto rounded-l-3xl object-cover" />
+            </div>
         </div>
-    )
+    );
+
 }
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -60,18 +60,20 @@ export default function RegisterPage() {
     }
 
     return (
-        <div className="h-full w-full flex justify-between bg-[#131619]">
-            <form onSubmit={onSubmit} className="flex flex-col justify-center items-center text-white w-full">
-                <div className="-mt-48 mb-48 w-[500px] flex flex-row items-center gap-5">
+        <div className="h-full w-full flex flex-col md:flex-row justify-between bg-[#131619]">
+            <form onSubmit={onSubmit} className="flex flex-col justify-center items-center text-white w-full h-full px-4 md:px-0">
+                <div className="mt-8 md:-mt-48 mb-12 md:mb-48 w-full md:w-[500px] flex flex-row items-center gap-3 justify-center md:justify-start">
                     <Image src="/images/logo.webp" alt="Logo" width={1024} height={1024} className="w-[40px] h-[40px] rounded-2xl" />
                     <p className="font-semibold text-2xl">SwiftSeats</p>
                 </div>
-                <div className="flex flex-col w-[500px] gap-20">
-                    <div className="flex flex-col gap-2">
-                        <h1 className="text-4xl font-semibold">Create an account!</h1>
-                        <p className="text-[#9B9C9E]">Get started to participate to any event!</p>
+
+                <div className="flex flex-col w-full md:w-[500px] gap-8 md:gap-20">
+                    <div className="flex flex-col gap-2 text-center md:text-left">
+                        <h1 className="text-3xl md:text-4xl font-semibold">Create an account!</h1>
+                        <p className="text-[#9B9C9E]">Get started to participate in any event!</p>
                     </div>
-                    <div className="flex flex-col w-[500px] gap-5">
+
+                    <div className="flex flex-col gap-5">
                         <div className="flex flex-col gap-1">
                             <p className="text-[#9B9C9E]">Email or username</p>
                             <Input type="text" name="username" placeholder="admin@fabulosu.xyz" className="text-black" ref={usernameRef} required={true} />
@@ -82,27 +84,17 @@ export default function RegisterPage() {
                         </div>
                         <div className="flex flex-col gap-1">
                             <p className="text-[#9B9C9E]">Confirm your password</p>
-                            <Input type="password" name="cpass" placeholder="************" className="text-black" ref={cpassRef} />
+                            <Input type="password" name="cpass" placeholder="************" className="text-black" ref={cpassRef} required={true} />
                         </div>
                         <Button type="submit" className="mt-5 bg-emerald-500 hover:bg-emerald-600">Create account</Button>
                     </div>
                 </div>
             </form>
-            <Image src="/images/illustration.webp" alt="Illustration" width={970} height={970} className="h-full w-full rounded-l-3xl" />
+
+            <div className="hidden lg:block w-auto lg:w-full">
+                <Image src="/images/illustration.webp" alt="Illustration" width={970} height={970} className="h-full w-full md:w-auto rounded-l-3xl object-cover" />
+            </div>
         </div>
-        // <div className="h-[83.5vh] w-full flex justify-center">
-        //     <form onSubmit={onSubmit} className="pt-60 md:pt-60 flex items-center text-primary flex-col w-full md:w-1/2">
-        //         <h1 className="font-bold text-3xl">Join Linktr.ee</h1>
-        //         <p className="text-muted-foreground">Sign up for free!</p>
-        //         <Separator className="w-1/2 mb-4 pt-1 mt-1" />
-        //         <Input type="text" name="username" placeholder="Username" className="w-5/6 md:w-2/4 mt-5 text-base font-semibold" ref={usernameRef} required={true} />
-        //         <Input type="email" name="email" placeholder="Email" className="w-5/6 md:w-2/4 mt-2 text-base font-semibold" ref={emailRef} />
-        //         <Input type="password" name="password" placeholder="Password" className="w-5/6 md:w-2/4 mt-2 text-base font-semibold" ref={passRef} />
-        //         <Input type="password" name="cpass" placeholder="Confirm password" className="w-5/6 md:w-2/4 mt-2 text-base font-semibold" ref={cpassRef} />
-        //         {errorMessage && <p className="w-5/6 md:w-2/4 text-center p-3 bg-red-400 rounded-md mt-2 text-red-600 font-bold">{errorMessage}</p>}
-        //         <Button type="submit" className="w-5/6 md:w-2/4 mt-2">Create account</Button>
-        //         <p className="pt-3 text-muted-foreground">You have an account? <a href="login" className="transition-all ease-linear duration-200 text-primary hover:text-muted-foreground">Login</a></p>
-        //     </form>
-        // </div>
-    )
+    );
+
 }
