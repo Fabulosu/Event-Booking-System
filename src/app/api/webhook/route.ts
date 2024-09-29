4423bc562497f9a4c1d43fed95ad44cfce878d7b
Edward <batalanedward@gmail.com>
2024-09-29 16:02:45 +0300

    removed api config export

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -8,12 +8,6 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
     apiVersion: '2024-06-20',
 });
 
-export const config = {
-    api: {
-        bodyParser: false,
-    },
-};
-
 export async function POST(req: NextRequest) {
 
     const headersList = headers();
