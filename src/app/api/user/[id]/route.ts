cb8c103d41aa22d0ad945cd3c219690ca3b399e9
Edward <batalanedward@gmail.com>
2024-11-17 13:14:22 +0200

    added return events and reviews

--- src/app/api/user/[id]/route.ts
+++ src/app/api/user/[id]/route.ts
@@ -1,5 +1,5 @@
 import { dbConnect } from "@/utils/database";
-import { UserModel } from "@/utils/models";
+import { EventModel, ReviewModel, UserModel } from "@/utils/models";
 import { NextResponse } from "next/server";
 
 export async function GET(req: Request, { params }: { params: { id: string } }) {
@@ -7,6 +7,11 @@ export async function GET(req: Request, { params }: { params: { id: string } }) 
 
     await dbConnect();
     const user = await UserModel.findOne({ _id: id });
-    if (user) return NextResponse.json({ success: true, user: user });
+    if (!user) {
+        return NextResponse.json({ success: false, message: "User not found" }, { status: 404 });
+    }
+    const events = await EventModel.find({ organizer: id });
+    const reviews = await ReviewModel.find({ user: id }).populate("event", "title").populate("user", "username");
+    if (user) return NextResponse.json({ success: true, user, events, reviews });
     else return NextResponse.json({ success: false, message: "This account doesn't exist!" }, { status: 200 });
 }
